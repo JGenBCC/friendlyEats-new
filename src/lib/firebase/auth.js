7b6b06b1a30fef63691ed3e882951a1bbde7af43
JGenBCC <jgenbcc@gmail.com>
2025-02-01 18:31:00 +0530

    Adding Google Authentication 2

--- src/lib/firebase/auth.js
+++ src/lib/firebase/auth.js
@@ -1,3 +1,24 @@
+/*const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA96-KQpNFMB50m007wi2H21-aBJXQ1JYo",
+  authDomain: "friendlyeats-codelab-8226b.firebaseapp.com",
+  projectId: "friendlyeats-codelab-8226b",
+  storageBucket: "friendlyeats-codelab-8226b.firebasestorage.app",
+  messagingSenderId: "323606449403",
+  appId: "1:323606449403:web:f111864eac377e5de3f45e",
+  measurementId: "G-PJB0B6L7P5"
+};
+
 import {
   GoogleAuthProvider,
   signInWithPopup,
@@ -26,4 +47,25 @@ export async function signOut() {
   } catch (error) {
     console.error("Error signing out with Google", error);
   }
+}
+
+async function fetchWithFirebaseHeaders(request) {
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const installations = getInstallations(app);
+  const headers = new Headers(request.headers);
+  const [authIdToken, installationToken] = await Promise.all([
+    getAuthIdToken(auth),
+    getToken(installations),
+  ]);
+  headers.append("Firebase-Instance-ID-Token", installationToken);
+  if (authIdToken) headers.append("Authorization", `Bearer ${authIdToken}`);
+  const newRequest = new Request(request, { headers });
+  return await fetch(newRequest);
+}
+
+async function getAuthIdToken(auth) {
+  await auth.authStateReady();
+  if (!auth.currentUser) return;
+  return await getIdToken(auth.currentUser);
 }
